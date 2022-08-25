3082b7340c436eaa406661f389490064977197d8
cawauchi6204 <cawauchi6204@gmail.com>
2022-08-26 08:23:17 +0900

    I: 自動テスト用コミット

--- pages/task-page.tsx
+++ pages/task-page.tsx
@@ -20,7 +20,7 @@ const TaskPage: React.FC<STATICPROPS> = ({ staticTasks }) => {
     fallbackData: staticTasks,
     revalidateOnMount: true
   })
-  if (error) return <span>Error!</span>
+  if (error) return <span>Eror!</span>
 
   return (
     <Layout title="Todos">
